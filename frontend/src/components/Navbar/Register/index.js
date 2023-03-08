8ef4455395f4999067bff5a8db4abec3d3003226
AnasMBajawi <anas.mohammad.bajawi@gmail.com>
2023-03-08 21:21:30 +0300

    11th commit login and register design refactor

--- frontend/src/components/Navbar/Register/index.js
+++ frontend/src/components/Navbar/Register/index.js
@@ -1,113 +1,179 @@
-import React, {useState} from "react";
-import { Link } from 'react-router-dom'
-import axios from "axios"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 import {
-    MDBBtn,
-    MDBContainer,
-    MDBCard,
-    MDBCardBody,
-    MDBInput,
-    MDBIcon,
-    MDBRow,
-    MDBCol,
-    MDBCheckbox
-  }
-  from 'mdb-react-ui-kit';
-  import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+  MDBBtn,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBIcon,
+  MDBRow,
+  MDBCol,
+  MDBCheckbox,
+} from "mdb-react-ui-kit";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 
 export const Register = () => {
-    
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [DoB, setDob] = useState("")
-    const [phoneNumber, setPhoneNumber] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const role = "64075efb9e44d62bd11f48bd"
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [DoB, setDob] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const role = "64075efb9e44d62bd11f48bd";
 
-    const postUser = () =>{
-        axios
-            .post("http://localhost:5000/user/register",{
-                firstName,
-                lastName,
-                DoB,
-                phoneNumber,
-                email,
-                password,
-                role
-            })
-            .then((res)=>{
-                console.log(res);
-            })
-            .catch((err)=>{
-                console.log(err);
-            })
-
-    } 
+  const postUser = () => {
+    axios
+      .post("http://localhost:5000/user/register", {
+        firstName,
+        lastName,
+        DoB,
+        phoneNumber,
+        email,
+        password,
+        role,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-    <MDBContainer fluid className='my-5'>
-
-      <MDBRow className='g-0 align-items-center'>
-        <MDBCol col='6'>
-
-          <MDBCard className='my-5 cascading-right' style={{background: 'hsla(0, 0%, 100%, 0.55)',  backdropFilter: 'blur(30px)'}}>
-            <MDBCardBody className='p-5 shadow-5 text-center'>
-
+    <MDBContainer fluid className="my-5">
+      <MDBRow className="g-0 align-items-center">
+        <MDBCol col="6">
+          <MDBCard
+            className="my-5 cascading-right"
+            style={{
+              background: "hsla(0, 0%, 100%, 0.55)",
+              backdropFilter: "blur(30px)",
+            }}
+          >
+            <MDBCardBody className="p-5 shadow-5 text-center">
               <h2 className="fw-bold mb-5">Sign up now</h2>
 
               <MDBRow>
-                <MDBCol col='6'>
-                  <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text'/>
+                <MDBCol col="6">
+                  <MDBInput
+                    wrapperClass="mb-4"
+                    label="First name"
+                    id="form1"
+                    type="text"
+                    onChange={(e)=>{setFirstName(e.target.value)}}
+                  />
                 </MDBCol>
 
-                <MDBCol col='6'>
-                  <MDBInput wrapperClass='mb-4' label='Last name' id='form2' type='text'/>
+                <MDBCol col="6">
+                  <MDBInput
+                    wrapperClass="mb-4"
+                    label="Last name"
+                    id="form2"
+                    type="text"
+                    onChange={(e)=>{setLastName(e.target.value)}}
+                  />
                 </MDBCol>
               </MDBRow>
 
-              <MDBInput wrapperClass='mb-4' label='Email' id='form3' type='email'/>
-              <MDBInput wrapperClass='mb-4' label='Password' id='form4' type='password'/>
+              <MDBInput
+                wrapperClass="mb-4"
+                label="date of birth"
+                id="form5"
+                type="date"
+                onChange={(e)=>{setDob(e.target.value)}}
+              />
 
-              <div className='d-flex justify-content-center mb-4'>
-                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />
+              <MDBInput
+                wrapperClass="mb-4"
+                label="phone Number"
+                id="form6"
+                type="text"
+                onClick={(e)=>{setPhoneNumber(e.target.value)}}
+              />
+
+              <MDBInput
+                wrapperClass="mb-4"
+                label="Email"
+                id="form3"
+                type="email"
+                onChange={(e)=>{setEmail(e.target.value)}}
+              />
+              <MDBInput
+                wrapperClass="mb-4"
+                label="Password"
+                id="form4"
+                type="password"
+                onChange={(e)=>{setPassword(e.target.value)}}
+              />
+
+              <div className="d-flex justify-content-center mb-4">
+                <MDBCheckbox
+                  name="flexCheck"
+                  value=""
+                  id="flexCheckDefault"
+                  label="Subscribe to our newsletter"
+                />
               </div>
 
-              <MDBBtn className='w-100 mb-4' size='md'>sign up</MDBBtn>
+              <MDBBtn className="w-100 mb-4" size="md" onClick={(e)=>{postUser()}}>
+                sign up
+              </MDBBtn>
 
               <div className="text-center">
-
                 <p>or sign up with:</p>
 
-                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                  <MDBIcon fab icon='facebook-f' size="sm"/>
+                <MDBBtn
+                  tag="a"
+                  color="none"
+                  className="mx-3"
+                  style={{ color: "#1266f1" }}
+                >
+                  <MDBIcon fab icon="facebook-f" size="sm" />
                 </MDBBtn>
 
-                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                  <MDBIcon fab icon='twitter' size="sm"/>
+                <MDBBtn
+                  tag="a"
+                  color="none"
+                  className="mx-3"
+                  style={{ color: "#1266f1" }}
+                >
+                  <MDBIcon fab icon="twitter" size="sm" />
                 </MDBBtn>
 
-                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                  <MDBIcon fab icon='google' size="sm"/>
+                <MDBBtn
+                  tag="a"
+                  color="none"
+                  className="mx-3"
+                  style={{ color: "#1266f1" }}
+                >
+                  <MDBIcon fab icon="google" size="sm" />
                 </MDBBtn>
 
-                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                  <MDBIcon fab icon='github' size="sm"/>
+                <MDBBtn
+                  tag="a"
+                  color="none"
+                  className="mx-3"
+                  style={{ color: "#1266f1" }}
+                >
+                  <MDBIcon fab icon="github" size="sm" />
                 </MDBBtn>
-
               </div>
-
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
 
-        <MDBCol col='6'>
-          <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" class="w-100 rounded-4 shadow-4"
-            alt="" fluid/>
+        <MDBCol col="6">
+          <img
+            src="https://hips.hearstapps.com/hmg-prod/images/2019-bentley-continental-gt-placement-1525814986.jpg"
+            class="w-100 rounded-4 shadow-4"
+            alt=""
+            fluid
+          />
         </MDBCol>
-
       </MDBRow>
-
     </MDBContainer>
   );
 };
