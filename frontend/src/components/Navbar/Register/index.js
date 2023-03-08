316f539517b72c9bc5203143a5ebac68968c45e7
AnasMBajawi <anas.mohammad.bajawi@gmail.com>
2023-03-08 20:46:49 +0300

    10th commit login and register design refactor

--- frontend/src/components/Navbar/Register/index.js
+++ frontend/src/components/Navbar/Register/index.js
@@ -1,6 +1,19 @@
 import React, {useState} from "react";
 import { Link } from 'react-router-dom'
 import axios from "axios"
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBCard,
+    MDBCardBody,
+    MDBInput,
+    MDBIcon,
+    MDBRow,
+    MDBCol,
+    MDBCheckbox
+  }
+  from 'mdb-react-ui-kit';
+  import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 export const Register = () => {
     
@@ -33,21 +46,68 @@ export const Register = () => {
     } 
 
   return (
-    <div>
-      <input type="text" placeholder="first Name" onChange={(e)=>{setFirstName(e.target.value)}}/>
-      <br/>
-      <input type="text" placeholder="Last Name" onChange={(e)=>{setLastName(e.target.value)}}/>
-      <br/>
-      <input className="date" type="date" placeholder="date of birth" onChange={(e)=>{setDob(e.target.value)}} />
-      <br/>
-      <input  type="text" placeholder="phone number" onClick={(e)=>{setPhoneNumber(e.target.value)}} />
-      <br/>
-      <input type="text" placeholder="email" onChange={(e)=>{setEmail(e.target.value)}} />
-      <br/>
-      <input type="password" placeholder="password" onChange={(e)=>{setPassword(e.target.value)}} /><br/>
-      <button onClick={(e)=>{postUser()}}>Register</button>
-      <br/>
-      <Link to="/login"><button>login</button> </Link>
-    </div>
+    <MDBContainer fluid className='my-5'>
+
+      <MDBRow className='g-0 align-items-center'>
+        <MDBCol col='6'>
+
+          <MDBCard className='my-5 cascading-right' style={{background: 'hsla(0, 0%, 100%, 0.55)',  backdropFilter: 'blur(30px)'}}>
+            <MDBCardBody className='p-5 shadow-5 text-center'>
+
+              <h2 className="fw-bold mb-5">Sign up now</h2>
+
+              <MDBRow>
+                <MDBCol col='6'>
+                  <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text'/>
+                </MDBCol>
+
+                <MDBCol col='6'>
+                  <MDBInput wrapperClass='mb-4' label='Last name' id='form2' type='text'/>
+                </MDBCol>
+              </MDBRow>
+
+              <MDBInput wrapperClass='mb-4' label='Email' id='form3' type='email'/>
+              <MDBInput wrapperClass='mb-4' label='Password' id='form4' type='password'/>
+
+              <div className='d-flex justify-content-center mb-4'>
+                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />
+              </div>
+
+              <MDBBtn className='w-100 mb-4' size='md'>sign up</MDBBtn>
+
+              <div className="text-center">
+
+                <p>or sign up with:</p>
+
+                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
+                  <MDBIcon fab icon='facebook-f' size="sm"/>
+                </MDBBtn>
+
+                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
+                  <MDBIcon fab icon='twitter' size="sm"/>
+                </MDBBtn>
+
+                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
+                  <MDBIcon fab icon='google' size="sm"/>
+                </MDBBtn>
+
+                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
+                  <MDBIcon fab icon='github' size="sm"/>
+                </MDBBtn>
+
+              </div>
+
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+
+        <MDBCol col='6'>
+          <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" class="w-100 rounded-4 shadow-4"
+            alt="" fluid/>
+        </MDBCol>
+
+      </MDBRow>
+
+    </MDBContainer>
   );
 };
