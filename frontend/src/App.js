d2dd0a944ba6ec87f59629704b9888bc09798bea
AnasMBajawi <anas.mohammad.bajawi@gmail.com>
2023-03-09 19:56:53 +0300

    12th commit saving token in the local storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
 import { Navbar } from "./components/Navbar";
-import { Register } from "./components/Navbar/Register";
-import { Login } from "./components/Navbar/Login"
+import { Register } from "./components/Register";
+import { Login } from "./components/Login";
 import { Routes, Route, Link } from "react-router-dom";
+import React, { useState, createContext } from "react";
+export const UserContext = createContext();
+
 function App() {
+  const [token, setToken] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Project 4 </h1>
-      </header>
-      <Navbar />
-      <Routes>
-        <Route path ="/register" element ={<Register/>}/>
-        <Route path = "/login" element = {<Login/>}/>
-      </Routes>
-    </div>
+    <UserContext.Provider value={setToken()}>
+      <div className="App">
+        <header className="App-header"></header>
+        <Navbar />
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </UserContext.Provider>
   );
 }
 
--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import "./index.css" 
 export const Navbar = () => {
+    const navigate = useNavigate()
   return (
-    <div>
-        <Link to = "/register"><button>rent my car</button></Link>
-        <Link to="/cars"><button>vhiecles</button></Link>
+    <div className='navbar'>
+        <button className='rent' onClick={(e)=>{navigate("/register")}}>rent my car</button>
+        <button className='rent1' onClick={(e)=>{navigate("/cars")}}>cars for rent</button>
+        <input class="search-bar" type="text" />
     </div>
   )
 }
